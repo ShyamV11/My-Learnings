0a6e0007e770fe92eacb90ba584bf05890346d44
ShyamV11 <vshyam93@gmail.com>
2019-12-09 18:49:38 +0530

    Add files via upload

---
+++ List_remove.py
@@ -0,0 +1,19 @@
+List = [1, 2, 3, 4, 5, 6,  
+        7, 8, 9, 10, 11, 12] 
+print("Intial List: ") 
+print(List) 
+  
+# Removing elements from List 
+# using Remove() method 
+List.remove(9) 
+List.remove(6) 
+print("\nList after Removal of two elements: ") 
+print(List) 
+  
+# Removing elements from List 
+# using iterator method 
+for i in range(1, 6): 
+    List.remove(i) 
+print("\nList after Removing a range of elements: ") 
+print(List) 
+
